cba4fd0ad06f82cf4b4d46ccb1d1ca73877a609d
duweike <604212506@qq.com>
2016-05-24 19:45:27 +0800

    sum of even , 1~100

---
+++ 1-leap-yaer.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+# 凡符合下面两个条件之一的年份是闰年。
+# 1.能被4整除但不能被100整除。
+# 2.能被400整除。
+year=int(input('Please input the year:'))
+if (year%4==0 and year%100 != 0) or (year%400 ==0) :
+    print year,'is a leap year'
+else:
+    print year,'is not a leap year'
